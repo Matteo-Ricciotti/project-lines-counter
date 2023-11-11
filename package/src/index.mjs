2080bf29c1785f88f37290bd0d54e1bb04cf3dea
Matteo <ricciottimatteo2003@gmail.com>
2023-11-11 12:35:45 +0100

    fix spinner

--- package/src/index.mjs
+++ package/src/index.mjs
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
 
 import chalk from 'chalk';
-import nanospinner from 'nanospinner';
 import promptPath from './utility/prompt-path.mjs';
-import checkOptions from './utility/check-options.mjs';
 import getAllFilesPaths from './utility/get-all-files-paths.mjs';
 import getAllFilesLines from './utility/get-all-files-lines.mjs';
 import calculateLines from './utility/calculate-lines.mjs';
+
+const calculate = (input) => {
+  const paths = getAllFilesPaths(input.path);
+
+  const filesLines = getAllFilesLines(paths);
+
+  const lines = calculateLines(filesLines);
+
+  return lines;
+};
+
+const onSuccess = (lines, time) => {
+  process.stdout.write('\u001B[1A');
+  process.stdout.clearLine();
+  console.log('Success!');
+
+  console.log(chalk.cyan(`Lines: ${chalk.greenBright(`${lines}`)}`));
+  console.log(chalk.cyan(`Time: ${chalk.greenBright(`${time}ms`)}`));
+};
 
 const init = async () => {
   // TO-IMPLEMENT
@@ -14,22 +31,15 @@ const init = async () => {
 
   const input = await promptPath();
 
+  console.log('Calculating...');
+
   const start = Date.now();
 
-  const spinner = nanospinner.createSpinner().start();
-
-  const paths = getAllFilesPaths(input.path);
-
-  const filesLines = getAllFilesLines(paths);
-
-  const lines = calculateLines(filesLines);
+  const lines = calculate(input);
 
   const end = Date.now();
 
-  spinner.stop();
-
-  console.log(chalk.cyan(`Lines: ${chalk.greenBright(`${lines}`)}`));
-  console.log(chalk.cyan(`Time: ${chalk.greenBright(`${end - start}ms`)}`));
+  onSuccess(lines, end - start);
 };
 
 init();
